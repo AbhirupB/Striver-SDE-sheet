1f31cde24c26ec1de8909dd08aa90a6c25055dab
Abhirup Bhattacharya <abhirupbhattacharya97@gmail.com>
2023-06-24 01:12:42 +0530

    day1 and day2

---
+++ arrays and matrix/pascal triangle.cpp
@@ -0,0 +1,133 @@
+/*
+variation 1: 
+In this case, we are given the row number r and the column number c, and we need to find out the element at position (r,c). 
+
+Use the combination formula: nCr = n!/(n-r)! * (r)!
+using the formula directly makes TC:O(n)+O(r)+O(n-r)
+
+We can optimize this calculation by the following observation. 
+Assume, given r = 7, c = 4. 
+Now, n = r-1 = 7-1 = 6 and r = c-1 = 4-1 = 3
+Let's calculate 6C3 = 6! / (3! *(6-3)!) = (6*5*4*3*2*1) / ((3*2*1)*(3*2*1))
+This will boil down to (6*5*4) / (3*2*1)
+So, nCr = (n*(n-1)*(n-2)*.....*(n-r+1)) / (r*(r-1)*(r-2)*....1)
+
+use this: (n / 1)*((n-1) / 2)*.....*((n-r+1) / r).
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+int pascalTriangle(int r, int c) {
+    int element = nCr(r - 1, c - 1);
+    return element;
+}
+
+int main()
+{
+    int r = 5; // row number
+    int c = 3; // col number
+    int element = pascalTriangle(r, c);
+    cout << "The element at position (r,c) is: "
+            << element << "n";
+    return 0;
+}
+// TC:O(c)  We are running a loop for r times, where r is c-1.
+// SC:O(1)  
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+void pascalTriangle(int n) {
+    // printing the entire row n:
+    for (int c = 1; c <= n; c++) {
+        cout << nCr(n - 1, c - 1) << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    int n = 5;
+    pascalTriangle(n);
+    return 0;
+}
+
+// TC:O(n*r), where n is the given row number, and r is the column index which can vary from 0 to n-1.
+//Reason: We are calculating the element for each column. 
+//Now, there are total n columns, and for each column, the calculation of the element takes O(r) time where r is the column index.
+
+
+
+//Variation 3: print the Pascal's triangle.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+
+vector<vector<int>> pascalTriangle(int n) {
+    vector<vector<int>> ans;
+
+    //Store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        vector<int> tempLst; // temporary list
+        for (int col = 1; col <= row; col++) {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 5;
+    vector<vector<int>> ans = pascalTriangle(n);
+    for (auto it : ans) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "n";
+    }
+    return 0;
+}
+
+//TC: O(n*n*r) ~ O(n^3), where n = number of rows, and r = column index.
+//Reason: The row loop will run for approximately n times. 
+//And generating a row using the naive approach of variation 2 takes O(n*r) time complexity.
+// SC:O(1)
---
+++ arrays and matrix/rotate matrix by 90deg.cpp
@@ -0,0 +1,103 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+vector < vector < int >> rotate(vector < vector < int >> & matrix) {
+    int n = matrix.size();
+    vector < vector < int >> rotated(n, vector < int > (n, 0));
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        rotated[j][n - i - 1] = matrix[i][j];
+    }
+    }
+    return rotated;
+}
+
+int main() {
+    vector < vector < int >> arr;
+    arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector < vector < int >> rotated = rotate(arr);
+    cout << "Rotated Image" << endl;
+    for (int i = 0; i < rotated.size(); i++) {
+    for (int j = 0; j < rotated[0].size(); j++) {
+        cout << rotated[i][j] << " ";
+    }
+    cout << "n";
+    }
+
+}
+// TC:O(n^2)
+// SC:O(n^2)
+
+
+/*
+Transpose the matrix itself and reverse each row.
+*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+void rotate(vector < vector < int >> & matrix) {
+    int n = matrix.size();
+    //transposing the matrix
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+        swap(matrix[i][j], matrix[j][i]);
+    }
+    }
+    //reversing each row of the matrix
+    for (int i = 0; i < n; i++) {
+    reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    vector < vector < int >> arr;
+    arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    rotate(arr);
+    cout << "Rotated Image" << endl;
+    for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr[0].size(); j++) {
+        cout << arr[i][j] << " ";
+    }
+    cout << "n";
+    }
+
+}
+// TC:O(n^2)
+// SC:O(1)
+
+
+
+/*
+FOR ROTAION PROBLEMS:
+
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+void rotate(vector<vector<int> > &matrix) {
+    reverse(matrix.begin(), matrix.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
+
+/*
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+*/
+void anti_rotate(vector<vector<int> > &matrix) {
+    for (auto vi : matrix) reverse(vi.begin(), vi.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
